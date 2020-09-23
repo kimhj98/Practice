200c76a33c41ad9bb66f5d2294bab0a6d774758d
kimhj98 <kimhjtiger@gmail.com>
2020-09-23 20:10:58 +0900

    python type test

--- typeChange.py
+++ typeChange.py
@@ -5,3 +5,26 @@ print(variable, type(variable))
 variable = "kim"
 
 print(variable, type(variable))
+
+print("git add ."
+      "git commit -m file name"
+      "git push origin master")
+
+variable = [1,2,3]
+print(type(variable), variable)
+
+variable = {'이름':'김현준', '나이':23,'키':'178'}
+
+print(type(variable), variable)
+print(variable['이름'])
+
+variable=('김현준','kimhj')
+
+print(type(variable))
+# tuple은 자료의 변경이 안됨
+
+print(variable[1])
+
+variable = True
+
+print(type(variable))
